75d694171d5d262aa6056d80251d4603bc32787a
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-01-11 13:46:50 +0545

    feat(introduction): Console Error & Process stderr node *.js 1>/dev/null 2>/dev/null

--- exercises/ex1.js
+++ exercises/ex1.js
@@ -1,5 +1,6 @@
 // My first Node.js program
 
-process.stdout.write('Hello world');
+// process.stdout.write('Hello world');
 
-// console.log('Hello world');
+console.log('Hello world');
+console.error('Something is wrong');
