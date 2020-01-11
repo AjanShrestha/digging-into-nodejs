79d366655b122eddbcd3479e50eb9124b77fa569
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-01-11 16:14:22 +0545

    feat(command-line): Argument Handling

--- exercises/ex1.js
+++ exercises/ex1.js
@@ -8,13 +8,29 @@ var args = require('minimist')(process.argv.slice(2), {
 });
 console.log(args);
 
-// printHelp();
+if (args.help) {
+  printHelp();
+} else if (args.file) {
+  console.log(args.file);
+} else {
+  error('Incorrect usage.', true);
+}
 
 // **********************
+
+function error(msg, includeHelp = false) {
+  console.error(msg);
+  if (includeHelp) {
+    console.log('');
+    printHelp();
+  }
+}
+
 function printHelp() {
   console.log('ex1 usage:');
-  console.log(' ex1.js --help');
+  console.log(' ex1.js --file={FILENAME}');
   console.log('');
   console.log('--help                 print this help');
+  console.log('--file={FILENAME}      process the file');
   console.log('');
 }
