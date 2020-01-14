bdefadb561fc4e08de03aa6015d63c2de93ad9ff
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-01-14 11:08:11 +0545

    feat(streams): File Handling with Streams

---
+++ exercises/ex2.js
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var util = require('util');
+var path = require('path');
+var fs = require('fs');
+
+var getStdin = require('get-stdin');
+
+var args = require('minimist')(process.argv.slice(2), {
+  boolean: ['help', 'in'],
+  string: ['file'],
+});
+
+var BASE_PATH = path.resolve(process.env.BASE_PATH || __dirname);
+
+if (args.help) {
+  printHelp();
+} else if (args.in || args._.includes('-')) {
+  processFile(process.stdin);
+} else if (args.file) {
+  let stream = fs.createReadStream(path.join(BASE_PATH, args.file));
+  processFile(stream);
+} else {
+  error('Incorrect usage.', true);
+}
+
+// **********************
+
+function processFile(inStream) {
+  var outStream = inStream;
+  var targetStream = process.stdout;
+  outStream.pipe(targetStream);
+}
+
+function error(msg, includeHelp = false) {
+  console.error(msg);
+  if (includeHelp) {
+    console.log('');
+    printHelp();
+  }
+}
+
+function printHelp() {
+  console.log('ex1 usage:');
+  console.log(' ex1.js --file={FILENAME}');
+  console.log('');
+  console.log('--help                 print this help');
+  console.log('--file={FILENAME}      process the file');
+  console.log('--in, -                process stdin');
+  console.log('');
+}
