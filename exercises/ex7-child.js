3ed464ce5240b09a79982e2ee781063e5b1dfad4
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-01-23 15:24:26 +1100

    feat(child-process):  Child Processes

---
+++ exercises/ex7-child.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// var fetch = require("node-fetch");
+
+// ************************************
+
+const HTTP_PORT = 8039;
+
+main()
+  .then(() => 0)
+  .catch(() => 1);
+
+// ************************************
+
+async function main() {}
